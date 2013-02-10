a8b82c7a49f00a1eeb470f2c69a7a472d00b0ad2
Martin Pettersson <kontakt@martinpettersson.se>
2013-02-10 01:56:09 +0100

    Included src

---
+++ src/Reversebinary.java
@@ -0,0 +1,29 @@
+import java.lang.StringBuffer;
+
+public class Reversebinary {
+	Kattio io;
+	long inputInteger;
+	long outInteger;
+	String binary;
+	String reverseBinary;
+	
+	public Reversebinary() {
+		io = new Kattio(System.in, System.out);
+		inputInteger = io.getInt();
+		binary = Long.toBinaryString(inputInteger);
+		
+		StringBuffer buffer = new StringBuffer(binary);
+		buffer = buffer.reverse();
+		reverseBinary = buffer.toString();
+		
+		outInteger = Long.parseLong(reverseBinary, 2);
+		
+		io.println(outInteger);
+		
+		io.close();		
+	}
+	
+	public static void main(String[] args) {
+		new Reversebinary();
+	}
+}
